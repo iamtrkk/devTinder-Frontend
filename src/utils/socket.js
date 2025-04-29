2938f9b366dcd3ecbf5f029fc785c7a2b2ce2927
iamtrkk <123228233+iamtrkk@users.noreply.github.com>
2025-04-30 01:30:57 +0530

    Socket path fix for cloud server

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,5 +2,8 @@ import io from "socket.io-client";
 import { BASE_URL } from "./const";
 
 export const createSocketConnection = () => {
-  return io(BASE_URL);
+  if (location.hostname === "localhost") return io(BASE_URL);
+  //Bcz socket opens default on path /socket.io and our backend is deployed on path /api
+  // so we need to provide path to open socket which backend can listen
+  else return io("/", { path: "/api/socket.io" });
 };
